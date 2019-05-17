fad8bd9fb431ea1ee19525996e5516656da0f72e
Raymon Francisco Guzmán Domínguez <rguzman@viva.com.do>
2019-05-16 23:24:11 -0400

    Comments in GraphQL

--- schema.js
+++ schema.js
@@ -25,20 +25,24 @@ type pedidos{
 type Query {
      getCliente(id: ID) : Cliente
  }
-    
+   
+ """ Categoría del cliente"""
 enum TipoCliente{
     CLASICO
     PREMIUM
 }
      
- 
+ """Pedidos, nombre del producto y precio del producto"""
 input pedidosInput{
     producto: String
     precio: Int
+    
 }
+
 input emailsInput{
     email:String
 }
+""" Campos para Insertar los clientes con sus tipos de datos"""
  input ClienteInput{
     id: ID
     nombre: String!
@@ -49,7 +53,10 @@ input emailsInput{
     pedido: [pedidosInput]
  }
 
+ """ Aqui el mutation para crear los clientes"""
  type Mutation {
+     #Nombre del Resolver, Input con Datos y Valor que Retorna
+     """Te Permite crear Nuevos Clientes """
     crearCliente(input: ClienteInput) : Cliente
  }
 `);
